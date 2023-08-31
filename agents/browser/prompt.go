3bb5126fa325e8aff076425e17552fc67e9f75cf
Simun Strukan <simun.strukan@gmail.com>
2023-08-31 23:59:22 +0200

    Added custom summarisation chain

--- agents/browser/prompt.go
+++ agents/browser/prompt.go
@@ -2,19 +2,29 @@ package browser
 
 import (
 	"context"
+	"fmt"
 	"gpt/tools/scraper"
 	"strings"
 
 	"github.com/tmc/langchaingo/chains"
 	"github.com/tmc/langchaingo/documentloaders"
 	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/prompts"
 	"github.com/tmc/langchaingo/schema"
 	"github.com/tmc/langchaingo/textsplitter"
 )
 
+const SummarisationTemplate = `
+	Write a concise summary of the following:
+
+	"{{.context}}"
+
+	CONCISE SUMMARY:`
+
 func Prompt(input string) string {
-	llm, err := openai.New(openai.WithModel("gpt-4"))
+	llm, err := openai.NewChat(openai.WithModel("gpt-4"))
 	if err != nil {
+		fmt.Println(err)
 		return ""
 	}
 
@@ -22,16 +32,22 @@ func Prompt(input string) string {
 
 	webDocuments, err := loadWebContent(ctx, input)
 	if err != nil {
+		fmt.Println(err)
 		return ""
 	}
 
-	summaryChain := chains.LoadStuffSummarization(llm)
+	llmChain := chains.NewLLMChain(llm, prompts.NewPromptTemplate(
+		SummarisationTemplate, []string{"context"},
+	))
+
+	summaryChain := chains.NewStuffDocuments(llmChain)
 	summary, err := chains.Call(
 		ctx,
 		summaryChain,
 		map[string]any{"input_documents": webDocuments},
 	)
 	if err != nil {
+		fmt.Println(err)
 		return ""
 	}
 
